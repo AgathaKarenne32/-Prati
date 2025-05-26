7d5c163c2cabb1d81d94558d35910d365fce0c09
Agatha Karenne <142344001+AgathaKarenne32@users.noreply.github.com>
2025-05-26 19:29:54 +0000

    atualizar aula15

--- aula15 - listas duplamente encadeadas.js
+++ aula15 - listas duplamente encadeadas.js
@@ -35,3 +35,58 @@ lista.inserirInicio(10);
 lista.inserirInicio(20);
 
 lista.imprimir()
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0
+    }
+
+    inserirInicio(v) {
+        let novo = new node(v);
+        novo.next = this.head;
+        if (this.head) {
+            this.head.prev = novo;
+        }
+        this.head = novo;
+    }
+
+    inserirFim(v) {
+        let novo = new node(v);
+        if (!this.head) {
+            this.head = novo;
+            this.tail = novo;
+        } else {
+            this.tail.next = novo;
+            novo.prev = this.tail;
+            this.tail = novo;
+        }
+        this.length++;
+    }
+
+    inserirAtualizacao(v, pos) {
+        if (pos < 0 || pos >= this.length) {
+            console.log("Posição inválida");
+            return;
+        }
+        let novo = new node(v);
+        if (pos === 0) {
+            this.inserirInicio(v);
+            return;
+        }
+        let atual = this.head;
+        for (let i = 0; i < pos - 1; i++) {
+            atual = atual.next;
+        }
+        novo.next = atual.next;
+        novo.prev = atual;
+        if (atual.next) {
+            atual.next.prev = novo;
+        } else {
+            this.tail = novo;
+        }
+        atual.next = novo;
+        this.length++;
+    }
+}
